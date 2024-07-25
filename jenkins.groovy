0e62c28f0611164ba812ee49be7120fcbfb70cd0
liviu2016 <liviur@yahoo.com>
2024-07-25 15:27:01 -0400

    Create jenkins.groovy

---
+++ jenkins.groovy
@@ -0,0 +1,28 @@
+pipeline {
+  agent any
+    
+  tools {nodejs "node"}
+    
+  stages {
+        
+    stage('Git') {
+      steps {
+        git 'https://github.com/liviu2016/notes-app-cicd/'
+      }
+    }
+     
+    stage('Build') {
+      steps {
+        sh 'npm install'
+         sh '<<Build Command>>'
+      }
+    }  
+    
+            
+    stage('Test') {
+      steps {
+        sh 'node test'
+      }
+    }
+  }
+}
